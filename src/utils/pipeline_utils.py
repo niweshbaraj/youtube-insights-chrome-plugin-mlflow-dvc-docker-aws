3fa69cc6c97ef0c398ef00147e3e7d6c9ff57abd
Niwesh Baraj <niweshbaraj@gmail.com>
2025-07-04 04:03:26 +0530

    fix pipeline_utils

--- src/utils/pipeline_utils.py
+++ src/utils/pipeline_utils.py
@@ -1,12 +1,16 @@
 import pandas as pd
+import numpy as np
+
 
 def select_clean_comment_column(X):
-    """Returns a 1D Series or list of clean_comment values."""
+
     if isinstance(X, pd.DataFrame):
-        return X["clean_comment"]  # returns all rows
+        return X["clean_comment"]
     elif isinstance(X, pd.Series):
         return X
     elif isinstance(X, list):
         return pd.Series(X)
+    elif isinstance(X, np.ndarray):
+        return pd.Series(X)
     else:
-        raise ValueError("Unsupported input type")
+        raise ValueError(f"Unsupported input type: {type(X)}")
