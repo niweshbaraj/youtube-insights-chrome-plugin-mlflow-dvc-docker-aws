2930336371ef97187dc0c6fbeac950fc7c8cdf18
Niwesh Baraj <niweshbaraj@gmail.com>
2025-07-04 10:39:42 +0530

    minor change in app.py for logging

--- server/app.py
+++ server/app.py
@@ -56,7 +56,7 @@ def load_model(model_name, stage="Production"):
         raise RuntimeError(f"No model for '{model_name}' in stage '{stage}'")
     
     model_version = latest[0].version
-    model_uri = f"models:/{model_name}/{model_version}"
+    model_uri = f"models:/{model_name}/{model_version, flush=True}"
     print(f"📦 Loading model {model_uri}")
     model = mlflow.pyfunc.load_model(model_uri)
     # print(model.metadata.signature)
